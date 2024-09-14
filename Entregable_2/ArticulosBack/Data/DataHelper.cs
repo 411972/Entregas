9e3cb375d00b18aa3b06f2e1d0b7b6d91a8cc804
Franco <francocatania93@gmail.com>
2024-09-14 15:53:25 -0300

    Se agregan el entregable 1 de la semana pasada y el entregable 2 para esta semana

---
+++ Entregable_2/ArticulosBack/Data/DataHelper.cs
@@ -0,0 +1,124 @@
+﻿using ArticulosBack.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticulosBack.Data
+{
+    public class DataHelper
+    {
+        private SqlConnection _connection;
+        private static DataHelper _instance;
+
+        private DataHelper(){
+
+            _connection = new SqlConnection("Data Source=DESKTOP-RCC7Q44\\SQLEXPRESS;Initial Catalog=facturaciones;Integrated Security=True;");
+        }
+
+        public static DataHelper GetInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new DataHelper();
+            }
+            return _instance;
+        }
+
+        public SqlConnection OpenConnection()
+        {
+            if(_connection.State == ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+            return _connection;
+        }
+
+        public SqlConnection CloseConnection()
+        {
+            if(_connection.State == ConnectionState.Open)
+            {
+                _connection.Close();
+            }
+            return _connection ;
+        }
+
+
+        public void CommitTransaction(SqlTransaction transaction)
+        {
+            transaction?.Commit();
+        }
+
+        public void RollbackTransaction(SqlTransaction transaction)
+        {
+            transaction?.Rollback();
+        }
+        public SqlTransaction beginTransaction()
+        {
+
+            return OpenConnection().BeginTransaction();
+        }
+
+        public DataTable ExecuteSPGet(string sp, List<SqlParameter>? lstP)
+        {
+            DataTable dt = new DataTable();
+            SqlCommand cmd = new SqlCommand(sp, _connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                if(lstP != null)
+                {
+                    foreach(SqlParameter p in lstP)
+                    {
+                        cmd.Parameters.AddWithValue(p.ParameterName, p.Value);
+                    }
+                }
+
+                dt.Load(cmd.ExecuteReader());
+            }
+            catch (Exception exc)
+            {
+
+                throw exc;
+            }
+
+            return dt;
+
+        }
+        
+        public bool ExecuteSPPost(string sp, List<SqlParameter> lstP, SqlTransaction t)
+        {
+            bool result = false;
+            int rows = 0;
+            SqlCommand cmd = new SqlCommand(sp, _connection, t);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            try
+            {
+                foreach( SqlParameter p in lstP)
+                {
+                    cmd.Parameters.AddWithValue(p.ParameterName, p.Value);
+                }
+
+                rows = cmd.ExecuteNonQuery();
+
+                if(rows > 0)
+                {
+                    result = true;
+                }
+
+            }
+            catch (Exception exc)
+            {
+
+                throw exc;
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ Entregable_2/ArticulosBack/Data/Repository/ArticuloRepository.cs
@@ -0,0 +1,157 @@
+﻿using ArticulosBack.Data.Contracts;
+using ArticulosBack.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticulosBack.Data.Repository
+{
+    internal class ArticuloRepository : IAplicacion
+    {
+        public bool AgregarArticulo(Articulo? a)
+        {
+            bool result = true;
+            SqlTransaction t = null;
+
+            try
+            {
+                t = DataHelper.GetInstance().beginTransaction();
+                List<SqlParameter> parametrosLst = new List<SqlParameter>();
+                SqlParameter nombreArt = new SqlParameter("@articulo", a.Nombre);
+                SqlParameter precioArt = new SqlParameter("@precio", a.precioUnitario);
+
+                parametrosLst.Add(nombreArt);
+                parametrosLst.Add(precioArt);
+
+                result = DataHelper.GetInstance().ExecuteSPPost("SP_AGREGAR_ARTICULO", parametrosLst, t);
+
+
+                DataHelper.GetInstance().CommitTransaction(t);
+
+            }
+            catch (Exception)
+            {
+                result = false;
+                DataHelper.GetInstance().RollbackTransaction(t);
+            }
+            finally
+            {
+                DataHelper.GetInstance().CloseConnection();
+            }
+
+
+            return result;
+        }
+
+        public bool EditarArticulo(Articulo? a)
+        {
+            bool result = true;
+            SqlTransaction t = null;
+
+            try
+            {
+                t = DataHelper.GetInstance().beginTransaction();
+                List<SqlParameter> parametrosLst = new List<SqlParameter>();
+                SqlParameter codArt = new SqlParameter("@id", a.Codigo);
+                SqlParameter nombreArt = new SqlParameter("@articulo", a.Nombre);
+                SqlParameter precioArt = new SqlParameter("@precio", a.precioUnitario);
+
+                parametrosLst.Add(codArt);
+                parametrosLst.Add(nombreArt);
+                parametrosLst.Add(precioArt);
+
+                result = DataHelper.GetInstance().ExecuteSPPost("SP_EDITAR_ARTICULO", parametrosLst, t);
+
+
+                DataHelper.GetInstance().CommitTransaction(t);
+
+            }
+            catch (Exception exc)
+            {
+                result = false;
+                DataHelper.GetInstance().RollbackTransaction(t);
+                throw exc;
+            }
+            finally
+            {
+                DataHelper.GetInstance().CloseConnection();
+            }
+
+
+            return result; ;
+        }
+
+        public bool EliminarArticulo(int id)
+        {
+            bool result = true;
+            SqlTransaction t = null;
+
+            try
+            {
+               t = DataHelper.GetInstance().beginTransaction();
+               List<SqlParameter> parametrosLst = new List<SqlParameter>();
+               SqlParameter codArt = new SqlParameter("@id", id);
+                parametrosLst.Add(codArt) ;
+
+                result = DataHelper.GetInstance().ExecuteSPPost("SP_ELIMINAR_ARTICULO", parametrosLst, t);
+
+                DataHelper.GetInstance().CommitTransaction(t);
+
+            }
+            catch (Exception exc)
+            {
+                result = false;
+                DataHelper.GetInstance().RollbackTransaction(t);
+                throw exc;
+            }
+
+            return result;
+        }
+
+        public List<Articulo> ObtenerArticulos(string? nombre)
+        {
+            List<SqlParameter> listP = new List<SqlParameter>();
+            List<Articulo> listA = new List<Articulo>();
+            DataTable list;
+            try
+            {
+                DataHelper.GetInstance().OpenConnection();
+                if (nombre != null)
+                {
+                    SqlParameter p = new SqlParameter("@nombre_articulo", nombre);
+                    listP.Add(p);
+                    list = DataHelper.GetInstance().ExecuteSPGet("SP_OBTENER_ARTICULOS", listP);
+                }
+
+                list = DataHelper.GetInstance().ExecuteSPGet("SP_OBTENER_ARTICULOS", listP);
+
+                foreach (DataRow r in list.Rows)
+                {
+                    Articulo oArticulo = new Articulo();
+                    oArticulo.Codigo = (int)r["id_articulo"];
+                    oArticulo.Nombre = (string)r["articulo"];
+                    oArticulo.precioUnitario = Convert.ToDouble(r["precioUnitario"]);
+
+                    listA.Add(oArticulo);
+                }
+
+
+            }
+            catch (Exception exc)
+            {
+
+                throw exc;
+            }
+
+            return listA;
+
+        }
+
+
+    }
+}
